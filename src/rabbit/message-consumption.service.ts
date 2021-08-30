a1c88d426157375434c055863f2827f0713cac3f
Audrius Butkus <audrius.butkus@telesoftas.com>
2021-08-30 19:20:09 +0300

    some refactoring

--- src/rabbit/message-consumption.service.ts
+++ src/rabbit/message-consumption.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { EventEmitter2 } from '@nestjs/event-emitter'
-import { Channel, ConsumeMessage } from 'amqplib'
+import { Channel } from 'amqplib'
+import { Queue } from './dto/queue.dto'
 import { RabbitConfig } from './dto/rabbit-config.dto'
 import { MessageAction } from './message-action'
 import { MESSAGE_RECEIVED } from './on-message-received'
@@ -19,24 +20,28 @@ export class MessageConsumptionService {
     for (const q of this.config.queues) {
       const eventName = MESSAGE_RECEIVED + q.queue
       if (this.emitter.hasListeners(eventName)) {
-        const listeners = this.emitter.listeners(eventName)
-        if (listeners.length > 0) {
-          this.channel.consume(q.queue, (msg) => {
-            const content = JSON.parse(msg.content.toString())
-            const contentToPublish = content.message ? content.message : content
-
-            const actions = new MessageAction(this.channel, msg)
-            try {
-              for (const listener of listeners) {
-                listener(contentToPublish, actions)
-              }
-              actions.acn()
-            } catch (err) {
-              actions.nack()
-            }
-          })
-        }
+        this.onListenersExist(eventName, q)
       }
+    }
+  }
+
+  private onListenersExist(eventName: string, q: Queue) {
+    const listeners = this.emitter.listeners(eventName)
+    if (listeners.length > 0) {
+      this.channel.consume(q.queue, (msg) => {
+        const content = JSON.parse(msg.content.toString())
+        const contentToPublish = content.message ? content.message : content
+
+        const actions = new MessageAction(this.channel, msg)
+        try {
+          for (const listener of listeners) {
+            listener(contentToPublish, actions)
+          }
+          actions.acn()
+        } catch (err) {
+          actions.nack()
+        }
+      })
     }
   }
 }
